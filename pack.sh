f3fec23b636e6d3c8e53be4ad38eb5715bfd5247
cloudshell <45150731+gitcloudshell@users.noreply.github.com>
2020-01-23 23:47:45 +0100

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,68 +1,29 @@
 
 #!/bin/bash
-# Demo-menu shell script
-## ----------------------------------
-# Define variables
-# ----------------------------------
-EDITOR=nano
-PASSWD=/etc/passwd
-RED='\033[0;41;30m'
-STD='\033[0;0;39m'
- 
-# ----------------------------------
-# User defined function
-# ----------------------------------
-pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
-}
+# mipass.sh Sample Script to read users password.
+# password storage
+data=$(tempfile 2>/dev/null)
 
-one(){
-	echo "one() called"
-        pause
-}
- 
-# do something in two()
-two(){
-	echo "two() called"
-        pause
-}
- 
-# function to display menus
-show_menus() {
-	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~"	
-	echo "  MENU-SOLOLINUX.ES  "
-	echo "~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. Set Terminal"
-	echo "2. Reset Terminal"
-	echo "3. Exit"
-}
-# Lee la accion sobre el teclado y la ejecuta.
-# Invoca el () cuando el usuario selecciona 1 en el menú.
-# Invoca a los dos () cuando el usuario selecciona 2 en el menú.
-# Salir del menu cuando el usuario selecciona 3 en el menú.
-read_options(){
-	local choice
-	read -p "Enter choice [ 1 - 3] " choice
-	case $choice in
-		1) one ;;
-		2) two ;;
-		3) exit 0;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
-	esac
-}
- 
-# ----------------------------------------------
-# Trap CTRL+C, CTRL+Z and quit singles
-# ----------------------------------------------
-trap '' SIGINT SIGQUIT SIGTSTP
- 
-# -----------------------------------
-# Main logic - infinite loop
-# ------------------------------------
-while true
-do
- 
-	show_menus
-	read_options
-done
+# trap it
+trap "rm -f $data" 0 1 2 5 15
+
+# get password
+dialog --title "Password" \
+--clear \
+--passwordbox "Introduzca su password" 10 30 2> $data
+
+ret=$?
+
+# make decision
+case $ret in
+  0)
+    echo "Password is $(cat $data)";;
+  1)
+    echo "Cancelar.";;
+  255)
+    [ -s $data ] &&  cat $data || echo "ESC pressed.";;
+esac
+Save and close the file. Run it as follows:
+
+chmod +x mipass.sh
+./mipass.sh
